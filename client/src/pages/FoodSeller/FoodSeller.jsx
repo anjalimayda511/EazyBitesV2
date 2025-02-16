a22bce321e705321d7921a414f95c0baaacd6de5
Abhishek Verma <abhishekaverma20@gmail.com>
2025-02-16 20:13:55 +0530

    minor

--- client/src/pages/FoodSeller/FoodSeller.jsx
+++ client/src/pages/FoodSeller/FoodSeller.jsx
@@ -188,7 +188,7 @@ const FoodSeller = () => {
                     <svg className="foodSellerDash-icon" viewBox="0 0 24 24" width="24" height="24">
                         <path d="M20 4H4v2h16V4zm1 10v-2l-1-5H4l-1 5v2h1v6h10v-6h4v6h2v-6h1zm-9 4H6v-4h6v4z" />
                     </svg>
-                    <span>My Store</span>
+                    <span>My Stall</span>
                 </motion.div>
 
                 <motion.div
