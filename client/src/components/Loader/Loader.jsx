86bcf89c03557433b293b747ae0124dc1e780b40
Abhishek Verma <abhishekaverma20@gmail.com>
2025-02-27 16:41:39 +0530

    loader improved

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -3,9 +3,9 @@ import { motion } from 'framer-motion';
 import "./Loader.css";
 
 const Loader = () => (
-    <div className="MyMenu-spinner-container">
+    <div className="spinner-container">
         <motion.div
-            className="MyMenu-spinner-dot"
+            className="spinner-dot"
             animate={{
                 scale: [1, 1.5, 1],
                 rotate: [0, 360],
@@ -18,7 +18,7 @@ const Loader = () => (
             }}
         />
         <motion.div
-            className="MyMenu-spinner-ring"
+            className="spinner-ring"
             animate={{ rotate: 360 }}
             transition={{
                 duration: 1.5,
